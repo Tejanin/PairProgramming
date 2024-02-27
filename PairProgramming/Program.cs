71443571b72e2ef2a2ec51f4ecdbf7d064d77551
Tejanin <theexama123@gmail.com>
2024-02-27 17:17:23 -0400

    Add some coments, added a for, and some testing

--- PairProgramming/Program.cs
+++ PairProgramming/Program.cs
@@ -8,41 +8,85 @@ namespace PairProgramming
 {
      public  class Program
      {
+        public static  int antsCounter = 0;
         static void Main(string[] args)
         {
 
-            CountAnts("..ant..ant.ant...ant.ant..ant.ant....ant..ant.ant.ant...ant..");
+            CountAnts("...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t");
             
         }
 
       public static int  CountAnts(string Ants )
-        {
-            //Console.WriteLine("Insert the Ants Line:");
-            //string AntsLine = Console.ReadLine();
+        {      
+               bool head = false;
+               bool body = false;
+               bool tail = false;
 
-            //Console.WriteLine("Insert the Ants Line after the Stampede:");
-            //string AntsLine = Console.ReadLine();
 
-            // O(2N)
-            
-
-            for (int i = 0; i < AntStampede.Length; i++)
+            for (int i = 0; i < Ants.Length; i++)
             {
-                if (AntStampede[i] == '.') continue;
+                // Confirm if the string is valid
+                if (Ants[i] != 'a' && Ants[i] != 'n' && Ants[i] != 't' && Ants[i] != '.') break;
+                // Skip all the dots
+                if (Ants[i] == '.') continue;
 
+                // If we find a living ant, we skip it
+                if (Ants.Length > i + 2 && Ants[i] == 'a' && Ants[i + 1] == 'n' && Ants[i + 2] == 't')
+                {
+                    i += 2;
+                    continue;
+                }
 
+                //Verify Every Part of the Ants
+                switch (Ants[i])
+                {
+                    case 'a':
+                        head = VerifyPart(head);
+                        break;
+                    case 'n':
+                        body = VerifyPart(body);
+                        break;
+                    case 't':
+                        tail = VerifyPart(tail);
+                        break;
+
+                }
+
+                //In case that is a whole body of ants, add 1 to the counter
+                if (head && body && tail)
+                {
+                    antsCounter++;
+
+                    head = false;
+                    body = false;
+                    tail = false;
+                }
+               
 
             }
 
+            //In case that a part of the ant is found, add 1 to the counter
+            if (head || body || tail)
+            {
+                antsCounter++;
+            }
 
-            int AliveBefore = Ants.Count(c => c == '.');
-            int AliveAfter = AntStampede.Count(c => c == '.');
-
-            int Deadants = AliveBefore - AliveAfter;
-            Console.WriteLine(Deadants);
-            Console.ReadLine();
-            return 0;
+            return antsCounter;
         }
 
-    }
+        private static bool VerifyPart(bool part)
+        {
+            if (part)
+            {
+                antsCounter ++;
+            }
+            else
+            {
+                part = true;
+            }
+
+            return part;
+        }
+
+     }
 }
--- PairProgrammingTesting/UnitTest1.cs
+++ PairProgrammingTesting/UnitTest1.cs
@@ -20,5 +20,21 @@ namespace PairProgrammingTesting
             Assert.AreEqual(3, PairProgramming.Program.CountAnts(AntStampede));
 
         }
+
+        [TestMethod]
+        public void TestingInvalidStrings()
+        {
+            string AntStampede = "...@ant4..85.ant.123.nat7.an6t2.t2..ant...ant..ant..ant.anant..t";
+
+            Assert.AreEqual(0, PairProgramming.Program.CountAnts(AntStampede));
+        }
+
+        [TestMethod]
+        public void TestingAnotherValidCase()
+        {
+            string AntStampede = "ant..a...antant...an.t.ant..ant.t.ant.a.n.t..";
+
+            Assert.AreEqual(4, PairProgramming.Program.CountAnts(AntStampede));
+        }
     } 
 }
