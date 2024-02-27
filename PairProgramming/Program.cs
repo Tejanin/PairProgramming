de4e0fe42f5d14804e7cc59c989fd72018761928
Tejanin <theexama123@gmail.com>
2024-02-27 16:05:33 -0400

    First mari commit

--- PairProgramming/Program.cs
+++ PairProgramming/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace PairProgramming
 {
-    internal class Program
-    {
+     public  class Program
+     {
         static void Main(string[] args)
         {
+            // O(2N)
+            string Ants = "..ant..ant.ant...ant.ant..ant.ant....ant..ant.ant.ant...ant..";
+            string AntStampede = "...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t";
+
+            int AliveBefore = Ants.Count(c => c == '.');
+            int AliveAfter = AntStampede.Count(c => c == '.');
+
+            int Deadants = AliveBefore - AliveAfter;
+            Console.WriteLine(Deadants);
+            Console.ReadLine();
+            
         }
+
+      public static int  CountAnts(string Ants )
+        {
+            return 0;
+        }
+
     }
 }
--- PairProgrammingTesting/UnitTest1.cs
+++ PairProgrammingTesting/UnitTest1.cs
@@ -3,9 +3,22 @@ namespace PairProgrammingTesting
     [TestClass]
     public class UnitTest
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void CountAliveAnts()
         {
+            string Ants = "..ant..ant.ant...ant.ant..ant.ant....ant..ant.ant.ant...ant..";
+           
+            Assert.AreEqual(0, PairProgramming.Program.CountAnts(Ants));
+           
         }
-    }
+        [TestMethod]
+        public void CountDeadAnts()
+        {
+            string AntStampede = "...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t";
+
+            Assert.AreEqual(3, PairProgramming.Program.CountAnts(AntStampede));
+
+        }
+    } 
 }
